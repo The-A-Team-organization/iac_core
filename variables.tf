7e911f468086fa17429fb9162757b6c3e2dd08f3
obulka <oleg.bulka@gmail.com>
2025-11-09 14:47:59 +0200

    TAT-86: add core Terraform configuration and providers

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "vpc_id" {
+  description = "Existing VPC id to use"
+  type        = string
+}
+
+variable "igw_id" {
+  description = "Optional Internet Gateway id in the VPC. If provided, modules will create a default 0.0.0.0/0 route in their route table. Leave empty to skip that route."
+  type        = string
+  default     = ""
+}
+
+variable "availability_zone" {
+  description = "AZ to place stage subnets/instances"
+  type        = string
+  default     = ""
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    CreatedBy   = "Terraform"
+    Project     = "Illuminati"
+    Environment = "stage"
+    Repository  = "https://github.com/The-A-Team-organization/illuminati_iac"
+    Module      = "stage_setup"
+  }
+}
+
+variable "jenkins" {
+  type = object({
+    ami           = string
+    instance_type = string
+    subnet_cidr   = string
+    allowed_cidrs = list(string)
+  })
+}
+
+variable "sonarqube" {
+  type = object({
+    ami           = string
+    instance_type = string
+    subnet_cidr   = string
+    allowed_cidrs = list(string)
+  })
+}
+
+variable "ecr_repositories" {
+  description = "List of repository names"
+  type        = list(string)
+  default     = ["frontend", "backend", "mailer_service", "scheduler_service"]
+}
+
