f1e10d6f4092f78b9b6214aa07dc4430dfd5db61
TarasAndreiko <TARAS.ANDREIKO@lnu.edu.ua>
2025-11-14 19:52:15 +0200

    TAT-86: update jenkins instance config with new root volume and userdata changes 2

--- modules/cross_account/main.tf
+++ modules/cross_account/main.tf
@@ -1,63 +1,139 @@
+# data "aws_iam_policy_document" "trust_jenkins" {
+#   statement {
+#     effect = "Allow"
+#
+#     principals {
+#       type        = "AWS"
+#       identifiers = [
+#         var.jenkins_role_arn,
+#         "arn:aws:iam::165015980393:user/ADMIN"
+#       ]
+#     }
+#
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
+#
+# resource "aws_iam_role" "ci_deploy_role" {
+#   name               = "${var.role_name}-${var.env}"
+#   assume_role_policy = data.aws_iam_policy_document.trust_jenkins.json
+#
+#   tags = {
+#     Environment = var.env
+#     CreatedBy   = "Terraform"
+#     Project     = "Illuminati"
+#   }
+# }
+#
+# data "aws_iam_policy_document" "default_perms" {
+#   statement {
+#     sid = "AllowEssentialOps"
+#
+#     actions = [
+#       "ec2:Describe*",
+#       "eks:DescribeCluster",
+#       "eks:ListClusters",
+#       "elasticloadbalancing:*",
+#       "autoscaling:*",
+#       "s3:GetObject",
+#       "s3:PutObject",
+#       "ssm:GetParameter",
+#       "ssm:GetParameters",
+#       "ecr:GetAuthorizationToken",
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:GetDownloadUrlForLayer",
+#       "ecr:BatchGetImage",
+#       "ecr:PutImage"
+#     ]
+#
+#     resources = ["*"]
+#   }
+# }
+#
+# resource "aws_iam_policy" "ci_deploy_policy" {
+#   name   = "${var.role_name}-policy-${var.env}"
+#   policy = data.aws_iam_policy_document.default_perms.json
+# }
+#
+# resource "aws_iam_role_policy_attachment" "attach_ci_policy" {
+#   role       = aws_iam_role.ci_deploy_role.name
+#   policy_arn = aws_iam_policy.ci_deploy_policy.arn
+# }
+#
+# output "ci_role_arn" {
+#   value = aws_iam_role.ci_deploy_role.arn
+# }
+#
+
+
+
+
+
+
+
 data "aws_iam_policy_document" "trust_jenkins" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "AWS"
-      identifiers = [var.jenkins_role_arn]
+      identifiers = [
+        var.jenkins_role_arn
+      ]
     }
 
     actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_role" "ci_deploy_role" {
-  name               = "${var.role_name}-${var.env}"
+
+resource "aws_iam_role" "terraform_role_stage" {
+  name               = "TerraformRoleStage"
   assume_role_policy = data.aws_iam_policy_document.trust_jenkins.json
-
-  tags = {
-    Environment = var.env
-    CreatedBy   = "Terraform"
-    Project     = "Illuminati"
-  }
 }
 
-data "aws_iam_policy_document" "default_perms" {
-  statement {
-    sid = "AllowEssentialOps"
 
-    actions = [
-      "ec2:Describe*",
-      "eks:DescribeCluster",
-      "eks:ListClusters",
-      "elasticloadbalancing:*",
-      "autoscaling:*",
-      "s3:GetObject",
-      "s3:PutObject",
-      "ssm:GetParameter",
-      "ssm:GetParameters",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:PutImage"
+resource "aws_iam_policy" "terraform_policy_stage" {
+  name        = "TerraformPolicyStage"
+  description = "Full access for Terraform in stage environment"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+      }
     ]
-
-    resources = ["*"]
-  }
+  })
 }
 
-resource "aws_iam_policy" "ci_deploy_policy" {
-  name   = "${var.role_name}-policy-${var.env}"
-  policy = data.aws_iam_policy_document.default_perms.json
+
+resource "aws_iam_role_policy_attachment" "attach_terraform_policy" {
+  role       = aws_iam_role.terraform_role_stage.name
+  policy_arn = aws_iam_policy.terraform_policy_stage.arn
 }
 
-resource "aws_iam_role_policy_attachment" "attach_ci_policy" {
-  role       = aws_iam_role.ci_deploy_role.name
-  policy_arn = aws_iam_policy.ci_deploy_policy.arn
+
+resource "aws_iam_policy" "jenkins_assume_terraform" {
+  name        = "JenkinsAssumeTerraform"
+  description = "Allow Jenkins to assume Terraform role"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = aws_iam_role.terraform_role_stage.arn
+      }
+    ]
+  })
 }
 
-output "ci_role_arn" {
-  value = aws_iam_role.ci_deploy_role.arn
-}
 
+resource "aws_iam_role_policy_attachment" "attach_jenkins_policy" {
+  role       = "jenkins_role_stage"  # example "jenkins_role_stage" !!!
+  policy_arn = aws_iam_policy.jenkins_assume_terraform.arn
+}
--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -95,7 +95,7 @@ resource "aws_instance" "jenkins" {
   subnet_id              = aws_subnet.jenkins_subnet.id
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.jenkins_profile.name
-  user_data              = var.user_data
+  # user_data              = var.user_data
 
   root_block_device {
     volume_size           = 15
